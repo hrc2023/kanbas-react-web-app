6d7abc781efd946eef131bc92d13db7514f539d1
hrc2023 <chen.haoyang4@northeastern.edu>
2024-10-03 21:08:08 -0700

    a2 CSS and Bootstrap

--- src/Kanbas/Courses/Navigation.tsx
+++ src/Kanbas/Courses/Navigation.tsx
@@ -11,7 +11,7 @@ export default function CoursesNavigation() {
       <a id="wd-neu-link" target="_blank" 
         href="https://www.northeastern.edu/"
         className="list-group-item bg-black border-0 text-center">
-        <img src="/images/NEU.png" width="75px" /></a>
+        <img src="/images/NEU.png" width="75px" alt="NEU"/></a>
     
         <Link
         to="/Kanbas/Account"
--- src/Labs/Lab2/Float.tsx
+++ src/Labs/Lab2/Float.tsx
@@ -11,8 +11,9 @@ const Float = () => {
      Blue </div>
    <div className="wd-float-left wd-dimension-portrait wd-bg-color-red">
      Red </div>
-   <img className="wd-float-right"
-     src="https://www.staradvertiser.com/wp-content/uploads/2021/08/web1_Starship-gap2.jpg"/>
+    <img className="wd-float-right" 
+    src="https://www.staradvertiser.com/wp-content/uploads/2021/08/web1_Starship-gap2.jpg" 
+    alt="Starship Image" />
    <div className="wd-float-done"></div>
  </div>
 </div>
