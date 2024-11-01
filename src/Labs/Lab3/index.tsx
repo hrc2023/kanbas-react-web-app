fc40cfa86a35f657c353bac41b581127ccd2c2c4
hrc2023 <156156731+hrc2023@users.noreply.github.com>
2024-11-01 03:05:45 -0700

    Update index.tsx

--- src/Labs/Lab3/index.tsx
+++ src/Labs/Lab3/index.tsx
@@ -1,9 +1,9 @@
 import VariablesAndConstants from "./VariablesAndConstants";
+
 export default function Lab3() {
   return(
     <div id="wd-lab3">
-      <h2>Lab 3</h2>
-      <h2>Haoyang (Hendrick) Chen</h2>
+      <h3>Lab 3</h3>
       <VariablesAndConstants/>
     </div>
   );
