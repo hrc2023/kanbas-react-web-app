68b78e3859d7d819dccc0b93fc349ec594e5c4e3
hrc2023 <156156731+hrc2023@users.noreply.github.com>
2024-11-01 02:36:53 -0700

    Update Profile.tsx

--- src/Kanbas/Account/Profile.tsx
+++ src/Kanbas/Account/Profile.tsx
@@ -1,9 +1,6 @@
 import { Link } from "react-router-dom";
-<<<<<<< HEAD
-export default function Profile() {
-=======
+
 export default function Profile(){
->>>>>>> 5389a6e (a1)
   return (
     <div id="wd-profile-screen" className="container">
       <h1>Profile</h1>
