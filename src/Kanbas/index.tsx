28cab606e2690f8a387b531e619d7ff03d468989
hrc2023 <chen.haoyang4@northeastern.edu>
2024-11-01 19:20:05 -0700

    a4

--- src/Kanbas/index.tsx
+++ src/Kanbas/index.tsx
@@ -38,17 +38,16 @@ export default function Kanbas() {
       <div className="wd-main-content-offset p-3">
         <Routes>
           <Route path="/" element={<Navigate to="Dashboard" />} />
-          <Route path="Account" element={<h1>Account</h1>} />
-          <Route path="Dashboard" element={
-            <ProtectedRoute><Dashboard
+          <Route path="Account/*"      element={<Account />} />
+          <Route path="Dashboard"      element={<ProtectedRoute>
+            <Dashboard
               courses={courses}
               course={course}
               setCourse={setCourse}
               addNewCourse={addNewCourse}
               deleteCourse={deleteCourse}
-              updateCourse={updateCourse}/></ProtectedRoute>
-          } />
-          <Route path="Courses/:cid/*" element={<ProtectedRoute><Courses courses={courses} /> </ProtectedRoute>} />
+              updateCourse={updateCourse}/></ProtectedRoute> } />
+          <Route path="Courses/:cid/*" element={<ProtectedRoute><Courses courses={courses} /></ProtectedRoute> } />
           <Route path="Calendar"       element={<h1>Calendar</h1>} />
           <Route path="Inbox"          element={<h1>Inbox</h1>} />
         </Routes>
